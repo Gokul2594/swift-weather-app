d135698cc4ce0b054c94f899eb1c7cde8452bfc7
Gokul <gokul2594@outlook.com>
2019-07-14 10:19:05 -0400

    Initial view created

--- WeatherViewer/ViewController.swift
+++ WeatherViewer/ViewController.swift
@@ -12,9 +12,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(headerLabel)
+        view.addSubview(searchTextField)
+        view.addSubview(searchButton)
+        view.setNeedsUpdateConstraints()
     }
 
+    lazy var headerLabel: UILabel! = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.font = view.font.withSize(36)
+        view.text = "Weather Viewer"
+        view.textAlignment = .center
+        
+        return view
+    }()
+    
+    lazy var searchTextField: UITextField! = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.placeholder = "City"
+        view.textAlignment = .left
+        view.font = .systemFont(ofSize: 24)
+        
+        return view
+    }()
+    
+    lazy var searchButton: UIButton! = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setTitle("Search", for: .normal)
+        view.backgroundColor = UIColor.white
+        view.setTitleColor(UIColor.black, for: .normal)
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.black.cgColor
+        
+        return view
+    }()
 
+    override func updateViewConstraints() {
+        let margins = view.layoutMarginsGuide
+        
+        headerLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 15).isActive = true
+        headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        searchTextField.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20).isActive = true
+        searchTextField.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 5).isActive = true
+        
+        searchButton.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20).isActive = true
+        searchButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 5).isActive = true
+        
+        super.updateViewConstraints()
+    }
 }
 
